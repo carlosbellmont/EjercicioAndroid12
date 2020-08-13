99719215d5184e5203ad18b940a89fcc33079379
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-13 18:01:29 +0200

    Initial commit

---
+++ app/src/main/java/com/cbellmont/ejemploandroid12/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.cbellmont.ejemploandroid12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+interface Clickable {
+    fun itemClicked(text: String)
+}
+
+class MainActivity : AppCompatActivity(), Clickable {
+
+
+    private lateinit var adapter : StringAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = MutableList(100) { pos -> "Elemento $pos"}
+        createRecyclerView(list)
+    }
+
+    private fun createRecyclerView(values: List<String>) {
+
+
+        adapter = StringAdapter(values, this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun itemClicked(text: String) {
+        textView2.text = text
+    }
+}
---
+++ app/src/main/java/com/cbellmont/ejemploandroid12/StringAdapter.kt
@@ -0,0 +1,33 @@
+package com.cbellmont.ejemploandroid12
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class StringAdapter(var stringList : List<String>, var listener : Clickable) : RecyclerView.Adapter<StringAdapter.StringViewHolder>()  {
+
+    class StringViewHolder(var root: View, var textView: TextView) : RecyclerView.ViewHolder(root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        val twTextView = view.findViewById<TextView>(R.id.textView)
+        return StringViewHolder(view, twTextView)
+    }
+
+    override fun getItemCount(): Int {
+        return stringList.size
+    }
+
+    override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
+        holder.textView.text = stringList[position]
+        holder.root.setOnClickListener {
+            Toast.makeText(holder.textView.context, "Has pulsado el elemento $position", Toast.LENGTH_LONG).show()
+            listener.itemClicked("Has pulsado el elemento $position")
+        }
+
+    }
+}
+
